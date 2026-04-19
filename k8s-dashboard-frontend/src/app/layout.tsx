628fc92b4fb4ed25e4a6cecae67abc2cf9127c1e
Bugra Aydogan <aydoganbugra93@gmail.com>
2026-04-19 20:23:34 +0300

    warning fix

--- k8s-dashboard-frontend/src/app/layout.tsx
+++ k8s-dashboard-frontend/src/app/layout.tsx
@@ -19,7 +19,7 @@ export default async function RootLayout({
   const messages = await getMessages();
 
   return (
-    <html lang={locale}>
+    <html lang={locale} suppressHydrationWarning>
       <head>
         {/* Prevent flash of wrong theme on initial load */}
         <script
